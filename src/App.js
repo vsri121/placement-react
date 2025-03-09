c34d89594003064a55c675608580706391ee1ec4
Varsha <srivarsha8625@gmail.com>
2025-03-09 11:33:31 +0530

    initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import TPO from "./components/TPO";
+import WhyRecruit from "./components/WhyRecruit";
+import Messages from "./components/Messages";
+import Achievements from "./components/Achievements";
+import PlacementProcess from "./components/PlacementProcess";
+import PlacementStatistics from "./components/PlacementStatistics";
+import PastRecruiters from "./components/PastRecruiters";
+import Contact from "./components/Contact";
+import "./assets/styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tpo" element={<TPO />} />
+        <Route path="/why-recruit" element={<WhyRecruit />} />
+        <Route path="/messages" element={<Messages />} />
+        <Route path="/achievements" element={<Achievements />} />
+        <Route path="/placement-process" element={<PlacementProcess />} />
+        <Route path="/placement-statistics" element={<PlacementStatistics />} />
+        <Route path="/past-recruiters" element={<PastRecruiters />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
